c015f93171033de350b2583fb0125a25c8336cf7
Patrick Hoydar <patrick@wallacegraphics.com>
2025-04-15 08:53:04 -0400

    add post endpoint

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
+router.post('/', (req, res) => {
+  return 'success';
+});
+
 module.exports = router;
